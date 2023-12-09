6466cb771960bb0ce3b88705a75f5840da321e31
Brian Garrity <brian.garrity26@outlook.com>
2023-12-09 12:13:44 -0600

    added show info for specific properties for checking and savings

--- src/main/java/org/bankAccount/Checking.java
+++ src/main/java/org/bankAccount/Checking.java
@@ -10,12 +10,23 @@ public class Checking extends Account {
     public Checking(String name, String sSN, double initDeposit) {
         super(name, sSN, initDeposit);
         accountNumber = "2" + accountNumber;
+        setDebitCard();
     }
+
     // List any methods specific to the checking account
+    private void setDebitCard() {
+        debitCardNumber = (int) (Math.random() * Math.pow(10, 12));
+        debitCardPin = (int) (Math.random() * Math.pow(10, 4));
+        System.out.println("CARD:" + this.debitCardNumber);
+        System.out.println("PIN:" + this.debitCardPin);
+    }
 
     @Override
     public void showInfo() {
         super.showInfo();
-        System.out.println("ACCOUNT TYPE: Checking");
+        System.out.println("Your Checking Account Features" +
+                "\nDebit Card Number: " + debitCardNumber +
+                "\nDebit Card PIN: " + debitCardPin
+                );
     }
 }
--- src/main/java/org/bankAccount/Savings.java
+++ src/main/java/org/bankAccount/Savings.java
@@ -11,12 +11,26 @@ public class Savings extends Account {
     public Savings(String name, String sSN, double initDeposit) {
         super(name, sSN, initDeposit);
         accountNumber = "1" + accountNumber;
+        setSafetyDepositBox();
+    }
 
+    private void setSafetyDepositBox() {
+        safetyDepositBoxId = (int) (Math.random() * Math.pow(10, 3));
+        safetyDepositBoxKey = (int) (Math.random() * Math.pow(10, 4));
+        System.out.println(safetyDepositBoxId);
     }
 
     // List any methods specific to savings account
     public void showInfo() {
-        super.showInfo();
         System.out.println("ACCOUNT TYPE: Saving");
+        super.showInfo();
+        System.out.println(
+                "Your Savings Account Features" +
+                        "\n Safety Deposit Box ID: " + safetyDepositBoxId +
+                        "\n Safety Deposit Box Key: " + safetyDepositBoxKey
+        );
+
     }
+
+
 }
