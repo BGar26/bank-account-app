b1e135e40a6acc926a0a99546b8c3ca68462ecdc
Brian Garrity <brian.garrity26@outlook.com>
2023-12-09 15:37:20 -0600

    finished printing info of new accounts from csv

--- src/main/java/org/bankAccount/BankAccountApp.java
+++ src/main/java/org/bankAccount/BankAccountApp.java
@@ -2,21 +2,35 @@ package org.bankAccount;
 
 import org.bankAccount.utlities.CSV;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class BankAccountApp {
     public static void main(String[] args) {
+        List<Account> accounts= new LinkedList<Account>();
+
+        // Read a CSV File then create new accounts based on that data
         String file = "C:\\Project files\\NewBankAccounts.csv\\";
-
-        // Read a CSV File then create new accounts based onthat data
         List<String[]> newAccountHolder = CSV.read(file);
-        for(String[] accountHolder : newAccountHolder) {
-            System.out.println("NEW ACCOUNT");
-            System.out.println(accountHolder[0]);
-            System.out.println(accountHolder[1]);
-            System.out.println(accountHolder[2]);
-            System.out.println(accountHolder[3]);
-
+        for (String[] accountHolder : newAccountHolder) {
+            System.out.println("NEW ACCOUNT FOR: ");
+            String name = accountHolder[0];
+            String sSN = accountHolder[1];
+            String accountType = accountHolder[2];
+            double initDeposit = Double.parseDouble(accountHolder[3]);
+            System.out.println(name + " " + sSN + " " + accountType + " " + initDeposit);
+            if (accountType.equals("Savings")) {
+                accounts.add(new Savings (name, sSN,initDeposit));
+            } else if (accountType.equals("Checking")) {
+                accounts.add(new Checking (name, sSN,initDeposit));
+            }
+            else{
+                System.out.println("ERROR READING ACCOUNT TYPE");
+            }
+        }
+        for(Account acc: accounts) {
+            System.out.println("\n-------------------------");
+          acc.showInfo();
         }
     }
 }
